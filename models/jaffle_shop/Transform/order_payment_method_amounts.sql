24795e8272a8b8d14b0ccb9046c5a3baa4baaaa2
avinashgoud.nallamasu@gmail.com <avinashgoud.nallamasu@gmail.com>
2023-02-08 10:19:38 +0000

    changes 08-Feb-2023

--- models/jaffle_shop/Transform/order_payment_method_amounts.sql
+++ models/jaffle_shop/Transform/order_payment_method_amounts.sql
@@ -1,14 +1,11 @@
-{{ config (materialized = 'table',schema='Transform') }}
+{{ config (materialized = 'table', schema = 'Transform') }}
 
-{% set payment_methods = ["bank_transfer", "credit_card", "gift_card", "Coupon"] %}
+{% set payment_methods =  get_payment_methods()  %}
 
 select
 order_id,
 {% for payment_method in payment_methods %}
 sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount,
 {% endfor %}
-sum(amount) as total_amount
-from {{ ref('raw_payments') }}
+sum(amount) as total_amount  from {{ ref('raw_payments') }}
 group by 1
-
-
